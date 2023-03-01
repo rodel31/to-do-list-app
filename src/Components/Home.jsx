2733bc8b3afd0162371ec5a4c00ff8069057117a
rodel31 <ramosrodel31lds@gmail.com>
2023-03-01 16:03:26 +0800

    modify Home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = ()=>{
     return(
-        <div>Hello Home</div>
+        <>
+            <h1>Welcome to My to do list</h1>
+            <button>start</button>
+        </>
     )
 }
 export default Home
